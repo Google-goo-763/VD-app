ef27219a729fcf92f8fc91504a7dab1293034430
[dyad] <git@dyad.sh>
2026-04-28 12:37:29 +0530

    [dyad] Setting 50/50 layout split - wrote 2 file(s)

--- src/components/editor/PreviewArea.tsx
+++ src/components/editor/PreviewArea.tsx
@@ -11,7 +11,7 @@ const PreviewArea = ({ aspectRatio }: PreviewAreaProps) => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
-    <div className="flex flex-col bg-black relative h-[45vh]">
+    <div className="flex flex-col bg-black relative flex-1">
       {/* Video Placeholder */}
       <div className="flex-1 flex items-center justify-center p-6">
         <div 
