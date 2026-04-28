8b677a1f58ab505d56ef5b05289f04d266a38c18
[dyad] <git@dyad.sh>
2026-04-28 16:10:12 +0530

    Reverted all changes back to version 713e34084a41e06b7f259bfb3d18a9a4e7493b40

--- src/components/editor/TopBar.tsx
+++ src/components/editor/TopBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React from 'react';
 import { X, Upload, ChevronDown, Monitor, Smartphone, Check, Save, Sparkles } from 'lucide-react';
 import {
   DropdownMenu,
